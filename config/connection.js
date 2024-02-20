64d0e68453c4823428d58e4d51df81757edd4149
Brianna Saglimbeni <briannabeni@Briannas-MacBook-Air.local>
2024-02-19 22:08:49 -0500

    added connection.js file and code

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ const { connect, connection } = require('mongoose');
 // Wrap Mongoose around local connection to MongoDB
 mongoose.connect('mongodb://127.0.0.1:27017/departmentsDB');
 
-// Connects Mongoose and MongoDB
+// Connects Mongoose and MongoDB using the connection string
 connect(connectionString);
 
 // Export connection 
