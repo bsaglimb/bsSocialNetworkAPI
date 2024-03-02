665ead80b40a3b5f0cf5d9fb3689646ce18e922e
Brianna Saglimbeni <briannabeni@Briannas-MacBook-Air.local>
2024-03-02 02:05:54 -0500

    added createReaction and deleteReaction

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -51,7 +51,7 @@ module.exports = {
             : res.json(user)
         )
         .catch((err) => res.status(500).json(err))
-    }
+    },
 
     //delete a thought
     deleteThought(req, res) {
@@ -70,6 +70,37 @@ module.exports = {
         ? res.status(404).json({ message: "Thought deleted"})
         : res.json({ message: "Thought successfully deleted" })
     )
+        .catch((err) => res.status(500).json(err));
+    },
+
+
+    //Create a reaction
+    createReaction(req, res) {
+        Thought.findOneAndUpdate(
+            { _id: req.params.thoughtId },
+            { $addToSet: { reactions: req.body } },
+            { runValidators: true, new: true }
+        )
+        .then((thought) =>
+            !thought
+            ? res.status(404).json({ message : "No thought found with this Id!"})
+            : res.json(thought)
+        )
+        .catch((err) => res.status(500).json(err));
+    },
+
+    //Delete a reaction
+    deleteReaction(req, res) {
+        Thought.findOneAndDelete(
+            { _id: req.params.thoughtId },
+            { $pull: { reactions: {reactionId: req.params.reactionId } } },
+            { runValidators: true, new: true }
+        )
+        .then((thought) =>
+        !thought
+        ? res.status(404).json({ message: "No thought found with this Id!"})
+        : res.json(thought)
+        )
         .catch((err) => res.status(500).json(err));
     },
 };
