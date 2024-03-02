f6432e44e56bf83c40d73bf1375415847a0d0d6b
Brianna Saglimbeni <briannabeni@Briannas-MacBook-Air.local>
2024-03-02 00:14:38 -0500

    added thoughtSchema to Thought.js

--- models/Thought.js
+++ models/Thought.js
@@ -33,3 +33,33 @@ const reactionSchema = new Schema (
         id: false,
     }
 )
+
+// thought schema
+
+const thoughtSchema = new Schema (
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minLength: 1,
+            maxLength: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => moment(createdAtVal).format("MMM DD, YYYY [at] hh:mm a"),
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [reactionSchema],
+        },
+        {
+            toJSON: {
+                virtuals: true,
+                getters: true,
+            },
+            id: false,
+        }
+)
