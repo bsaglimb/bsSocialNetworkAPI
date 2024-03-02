bedf5187be05e6e3d518182df77f671c9b86e957
Brianna Saglimbeni <briannabeni@Briannas-MacBook-Air.local>
2024-03-02 01:11:49 -0500

    added update a user and delete a user in userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -41,10 +41,23 @@ updateUser(req, res) {
         { runValidators: true, new: true }
     )
     .then((user) => 
-        !user? res.status(404).json ({ message: "No user found with that ID!"})
+        !user
+        ? res.status(404).json ({ message: "No user found with that ID!"})
         : res.json(user)
     )
     .catch((err) => res.status(500).json(err));
 },
 
-}
+// Delete a user
+deleteUser(req, res) {
+    User.findOneAndDelete({ _id: req.params.userId })
+    .then((user) =>
+        !user
+        ? res.status(404).json ({ message: "No user found with that ID!"})
+        : Thought.deleteMany({ _id: { $in: user.thoughts } })
+    )
+    .then(() => res.json({ message: "User and thought deleted!" }))
+    .catch((err) => res.status(500).json(err));
+},
+
+};
