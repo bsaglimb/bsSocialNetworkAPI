629651bdae884e16af47f2f548103c02a9775075
Brianna Saglimbeni <briannabeni@Briannas-MacBook-Air.local>
2024-03-08 02:14:01 -0500

    fixed type error

--- models/User.js
+++ models/User.js
@@ -1,48 +1,48 @@
-const { Schema, model} = require('mongoose');
+const { Schema, model } = require("mongoose");
 
 const userSchema = new Schema(
-    {
-        username: {
-            type: String,
-            unique: true,
-            required: true,
-            trim: true,
-        },
-        email: {
-            type: String,
-            required: true,
-            unique: true,
-            match: [
-                /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-                "Please type a valid email address",
-            ],
-        },
-        thoughts: [
-            {
-                types: Schema.Types.ObjectId,
-                ref: "Thought",
-            },
-        ],
-        friends: [
-            {
-                type: Schema.Types.ObjectId,
-                ref: "User",
-            },
-        ],
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
     },
-            {
-        toJSON: {
-            virtuals: true,
-        },
-        id: false,
-        }
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please type a valid email address",
+      ],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
 );
 
 userSchema.virtual("friendCount").get(function () {
-    return this.friends.length;
+  return this.friends.length;
 });
 // create the User model using the UserSchema
-const User = model('User', userSchema);
+const User = model("User", userSchema);
 
 // export the User model
 module.exports = User;
