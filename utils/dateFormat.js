a3b751c2adab4c0cfafbc37a1da7c5f5a49c8ed6
Brianna Saglimbeni <briannabeni@Briannas-MacBook-Air.local>
2024-03-08 21:34:13 -0500

    created formattedTimeStamp

--- utils/dateFormat.js
+++ utils/dateFormat.js
@@ -37,6 +37,34 @@ module.exports = (
         10: monthLength === 'short' ? 'Nov' : 'November',
         11: monthLength === 'short' ? 'Dec' : 'December',
     };
+
+    const dateObj = new Date(timestamp);
+    const formattedMonth = months[dateObj.getMonth()];
+
+    const dayOfMonth = dateSuffix
+    ? addDateSuffix(dateObj.getDate())
+    : dateObj.getData();
+
+    const year = dateObj.getFullYear();
+    let hour = 
+    dateObj.getHours() > 12
+    ? Math.floor(dateObj.getHours() - 12)
+    : dateObj.getHours();
+
+// if the hour is 0 (12:00am), change it to 12
+if (hour === 0){
+    hour = 12;
+}
+
+const minutes = (dateObj.getMinutes() < 10 ? '0' : '') + dateObj.getMinutes();
+
+// set 'am' or 'pm'
+const timeOfDay = dateObj.getHours() >= 12 ? 'pm' : 'am';
+
+const formattedTimeStamp = `${formattedMonth} ${dayOfMonth}, ${year}, at ${hour}:${minutes} ${timeOfDay}`;
+
+return formattedTimeStamp
+
 }
 
 
